f3f016ce74c5a1f276d80a6b43f95697312613f4
y.yuma0313@gmail.com <ec2-user@ip-172-31-87-202.ec2.internal>
2021-04-06 08:59:07 +0000

    sampleapp

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '1039d0f3440f8ede7ed7afa7fa88a71df793890747169c82b0f064f9c90ca2e69da4c00f2fd1185b513f2768cfdf93d5c0b3215886ff296772083fdf46222008'
